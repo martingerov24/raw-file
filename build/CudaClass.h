5796d20575a129593773347e1806832b98a3c9c5
unknown <mgerov@software-supreme.com>
2021-12-17 18:30:47 +0200

    TODO:: CudaKeypoints look for a way to get the reference and make the kernel function return a float

--- build/CudaClass.h
+++ build/CudaClass.h
@@ -1,10 +1,70 @@
 #pragma once
-#define THREADS_PER_BLOCK 1024
+#define THREADS_PER_BLOCK 32
 #include "cuda_runtime.h"
+#include "cuda/std/cmath"
 #include "device_launch_parameters.h"
 #include <vector>
 #include <cassert>
 #include <chrono>
+
+
+class CudaKeypoints
+{
+public:
+	CudaKeypoints(float2* keypoints, std::vector<uint8_t> data, uint8_t*& d_desc,float2*& d_keypoints, int& width, int &height)
+		:keypoints(keypoints), d_keypoints(d_keypoints), height(height), width(width)
+	{
+		cudaStatus = cudaError_t(0);
+		size = height * width;
+		cudaDeviceSetCacheConfig(cudaFuncCachePreferShared);
+		cudaStatus = cudaSetDevice(0);
+		assert(cudaStatus == cudaSuccess, "you do not have cuda capable device!");
+		cudaStatus = cudaStreamCreate(&stream);
+	}
+
+	__host__
+		void startup(int size)
+	{
+		cudaStatus = cudaMalloc((void**)&d_keypoints, size * sizeof(uint8_t));//sizeof(uint16_t) * size
+		assert(cudaStatus == cudaSuccess, "cudaMalloc failed!");
+
+		cudaStatus = cudaMalloc((void**)&d_desc, sizeof(uint8_t) * size);
+		assert(cudaStatus == cudaSuccess, "cudaMalloc failed!");
+	}
+	__host__
+	void Kernel(uint8_t* __restrict__ descriptors, const float2* __restrict__ keypoints,
+			const uint8_t* __restrict__ image, uint16_t x_res, uint16_t y_res);
+
+	__host__
+		void sync()
+	{
+		cudaStatus = cudaStreamSynchronize(stream);
+	}
+
+	__host__
+		~CudaKeypoints()
+	{
+		cudaFree(d_desc);
+		cudaFree(d_keypoints);// it was said to -> cudaFree ( void* devPtr )Frees memory on the device.
+		cudaStreamDestroy(stream);
+	}
+private:
+	uint8_t* &d_desc;
+	float2*& d_keypoints;
+
+	float2* &keypoints;
+	const std::vector<uint8_t>& img; 
+	const int& height, & width;
+	int size;
+	cudaStream_t stream;
+	cudaError_t cudaStatus;
+};
+
+
+
+
+
+
 class Cuda
 {
 public:
@@ -21,6 +81,7 @@ public:
 		assert(cudaStatus == cudaSuccess, "you do not have cuda capable device!");
 		cudaStatus = cudaStreamCreate(&stream);
 	}
+	
 	__host__
 		void startup(int size)
 	{
@@ -34,7 +95,7 @@ public:
 	void rawValue();
 
 	__host__
-		void sync()
+	void sync()
 	{
 		cudaStatus = cudaStreamSynchronize(stream);
 	}
